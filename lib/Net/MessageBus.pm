dc1c5f3cda6a019846d4d7de68dfc70c4fb2bbff
Horea Gligan <horea.gligan@evozon.com>
2012-03-13 19:36:48 +0200

    Version bump

--- lib/Net/MessageBus.pm
+++ lib/Net/MessageBus.pm
@@ -10,11 +10,11 @@ Net::MessageBus - Pure Perl simple message bus
 
 =head1 VERSION
 
-Version 0.05
+Version 0.06
 
 =cut
 
-our $VERSION = '0.05';
+our $VERSION = '0.06';
 
 use base 'Net::MessageBus::Base';
 
